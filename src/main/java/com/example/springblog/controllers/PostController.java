f21d54389f03891b737378f9d2356582aaf194ee
Anthony Stevens <aastevens1126@gmail.com>
2022-11-18 15:01:58 -0600

    spring views exercise complete/modified

--- src/main/java/com/example/springblog/controllers/PostController.java
+++ src/main/java/com/example/springblog/controllers/PostController.java
@@ -1,24 +1,44 @@
 package com.example.springblog.controllers;
 
+import com.example.springblog.models.Post;
+//import com.example.springblog.respositories.PostRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
+//    private final PostRepository postDao;
+//
+//    public PostController(PostRepository postDao) {
+//        this.postDao = postDao;
+//    }
 
     @GetMapping("/posts")
-    @ResponseBody
-    private String postsIndex(){
-        return "Posts index page";
+    private String postsIndex(Model model){
+        List<Post> posts = List.of(
+                new Post(1,"1 post","1 body"),
+                new Post(2,"2 post","2 body"),
+                new Post(3,"3 post","3 body"),
+                new Post(4,"4 post","4 body")
+        );
+
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
+
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    private String postsId(@PathVariable int id){
-        return "view an individual post "+id;
+    private String postsId(@PathVariable int id,Model model){
+        Post post = new Post(id,"first","My first post");
+        model.addAttribute("post",post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
---
+++ src/main/java/com/example/springblog/models/Post.java
@@ -0,0 +1,50 @@
+package com.example.springblog.models;
+
+//import javax.persistence.*;
+
+//@Entity
+public class Post{
+
+    public Post() {
+    }
+
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+//    @Column(nullable = false)
+    private String title;
+
+//    @Column(nullable = false)
+    private String body;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
